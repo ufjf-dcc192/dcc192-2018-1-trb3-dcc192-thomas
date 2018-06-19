2a4e05827065dbaaaf8e80214fe88d27350acd68
Thomas Santos <thomasbsjf@gmail.com>
2018-06-19 17:48:55 -0300

    Criação Class comentário

---
+++ src/java/dcc192/ufjf/br/Comentario.java
@@ -0,0 +1,11 @@
+package dcc192.ufjf.br;
+
+import java.util.Date;
+
+public class Comentario {
+    private Usuario proprietario;
+    private String texto;
+    private Date dataCriacao;
+    private Date dataAtualizacao;
+    private int avaliacao;
+}
