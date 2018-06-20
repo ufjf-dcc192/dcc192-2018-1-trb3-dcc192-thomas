a205ffd6d310272b922e7521b43a764e08c91053
Thomas Santos <thomasbsjf@gmail.com>
2018-06-19 22:43:44 -0300

    Gets + Setters Classe Comentario

--- src/java/dcc192/ufjf/br/Comentario.java
+++ src/java/dcc192/ufjf/br/Comentario.java
@@ -1,5 +1,6 @@
 package dcc192.ufjf.br;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Comentario {
@@ -8,4 +9,48 @@ public class Comentario {
     private Date dataCriacao;
     private Date dataAtualizacao;
     private int avaliacao;
+
+    public Comentario() {
+        dataCriacao = new Date(System.currentTimeMillis());
+    }
+
+    public Usuario getProprietario() {
+        return proprietario;
+    }
+
+    public void setProprietario(Usuario proprietario) {
+        this.proprietario = proprietario;
+    }
+
+    public String getTexto() {
+        return texto;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public Date getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(Date dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public Date getDataAtualizacao() {
+        return dataAtualizacao;
+    }
+
+    public void setDataAtualizacao(Date dataAtualizacao) {
+        this.dataAtualizacao = dataAtualizacao;
+    }
+
+    public int getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(int avaliacao) {
+        this.avaliacao = avaliacao;
+    }
 }
